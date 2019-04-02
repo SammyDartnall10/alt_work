54189ab31ec2010f323db34dacc32386ac0f9398
Sammy Dartnall <samantha.megan10@gmail.com>
2019-04-02 16:05:53 +0000

    working view_location

--- app.py
+++ app.py
@@ -19,22 +19,38 @@ def landing_page():
 """show all records in a grid - user can then filter by category etc"""     
 @app.route('/view_all')
 def view_all():
-    return render_template('viewall.html',
-    locations=mongo.db.locations.find())
+    return render_template("viewall.html",
+    location=mongo.db.locations.find())
     
+@app.route('/location')
+def location():
+    return render_template("location.html")
+   
 """show all records in a grid - user can then filter by category etc"""    
 @app.route('/show_results')
 def show_results():
     return render_template('searchresults.html')
-    
-"""display an individuial location after it is clicked by user"""    
+
 @app.route('/view_location/<location_id>')
 def view_location(location_id):
-    the_location = mongo.db.location.find_one({"_id": ObjectId(location_id)})
-    all_regions = mongo.db.region.find()
-    return render_template('location.html', location=the_location, area=all_regions)
+    single_location = mongo.db.locations.find_one({"_id": ObjectId(location_id)})
+    all_regions = mongo.db.regions.find()
+    return render_template('location.html', location=single_location, region=all_regions)
     
- 
+    
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_catergories = mongo.db.categories.find()
+    return render_template('edittask.html', task=the_task, categories=all_catergories)
+    
+    
+    
+    
+    
+    
+    
+    
 """add a new record for location - opens a form"""    
 @app.route('/add_new')
 def add_new():
