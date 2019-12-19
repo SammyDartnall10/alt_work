55e38183eb3e92a09bd09f024b646956d3ede932
Samantha Dartnall <samanthadartnall@Samanthas-MacBook-Pro.local>
2019-12-18 19:19:46 -0700

    google maps initial code added. Changing layout of location page

--- static/js/main.js
+++ static/js/main.js
@@ -5,3 +5,73 @@ $(".dropdown-search").on("click", function() {
     
 });
 
+/*---------Places API - Google------------------------------------------------*/
+
+var map;
+var service;
+var infowindow;
+var geocoder;
+var markers;
+var endLocation;
+
+
+function initMap() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(-41.28664, 174.77557);
+    var mapOptions = {
+        zoom: 13,
+        center: latlng
+    };
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    
+    var input = document.getElementById("companyCity");
+        var city = input.innerHTML;
+        
+        console.log(city);
+        console.log("started map function");
+
+        /*geocoding API from Google */
+
+        geocoder.geocode({ 'address': city }, function(results, status) {
+
+            if (status == 'OK') {
+                var location = {};
+
+                location['lat'] = results[0].geometry.location.lat();
+                location['lng'] = results[0].geometry.location.lng();
+
+                map.setCenter(results[0].geometry.location);
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: results[0].geometry.location
+
+                });
+                markers.push(marker);
+
+                marker.addListener('click', function() {
+                    map.setZoom(16);
+                    map.setCenter(marker.getPosition());
+                    endLocation = marker.getPosition();
+                    var infowindow = new google.maps.InfoWindow({
+                        content: '<p>Marker Location:' + marker.getPosition() + '</p>'
+                    });
+
+                    google.maps.event.addListener(marker, 'click', function() {
+                        infowindow.open(map, marker);
+                    });
+                });
+
+                location['marker'] = marker;
+
+                /*selection.geolocation global variable - pass the lat and lng to the object declared at beginning of code*/
+
+                selection.geolocation = location;
+
+                return location;
+                //console logging to make sure code is running.. 
+            }
+            else {
+                alert('Geocode was not successful for the following reason: ' + status);
+            }
+        });
+}
