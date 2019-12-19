86b70468d2d61652423813039eda904b8caee7bb
Samantha Dartnall <samanthadartnall@Samanthas-MacBook-Pro.local>
2019-12-18 21:20:37 -0700

    distance between two hardcoded lat/lng points working

--- static/js/main.js
+++ static/js/main.js
@@ -55,12 +55,6 @@ function initMap() {
                     };
                         console.log("Hi");
                         console.log(pos);
-
-
-                    /*infoWindow.setPosition(pos);
-                    infoWindow.setContent('Location found.');
-                    infoWindow.open(map);
-                    map.setCenter(pos);*/
                     }, 
                     
                     function() {
@@ -69,6 +63,51 @@ function initMap() {
                 } else {
                     // Browser doesn't support Geolocation
                     handleLocationError(false, infoWindow, map.getCenter());
+                }
+
+
+                /*---------Places API - Distances Matrix ------------------------------------------------*/
+
+                var origin1 = new google.maps.LatLng(55.930385, -3.118425);
+                
+                var destinationA = new google.maps.LatLng(50.087692, 14.421150);
+               
+
+                var service = new google.maps.DistanceMatrixService();
+                service.getDistanceMatrix(
+                {
+                    origins: [origin1],
+                    destinations: [destinationA],
+                    travelMode: 'DRIVING',
+                    unitSystem: google.maps.UnitSystem.METRIC,
+                    avoidHighways: false,
+                    avoidTolls: false
+                }, callback);
+
+
+                function callback(response, status) {
+                // See Parsing the Results for
+                // the basics of a callback function.
+
+                    if (status == 'OK') {
+                    var origins = response.originAddresses;
+                    console.log(origins)
+                    var destinations = response.destinationAddresses;
+                    console.log(destinations)
+                
+                    for (var i = 0; i < origins.length; i++) {
+                        var results = response.rows[i].elements;
+                        for (var j = 0; j < results.length; j++) {
+                        var element = results[j];
+                        var distance = element.distance.text;
+                        console.log(distance)
+                        var duration = element.duration.text;
+                        console.log(duration)
+                        var from = origins[i];
+                        var to = destinations[j];
+                        }
+                    }
+                    }
                 }
                 
                 return location;
@@ -80,6 +119,54 @@ function initMap() {
         });
 }
 
+
+
+
+
+/*---------Places API - Distances Matrix ------------------------------------------------*/
+
+var origin1 = new google.maps.LatLng(55.930385, -3.118425);
+var origin2 = 'Greenwich, England';
+var destinationA = 'Stockholm, Sweden';
+var destinationB = new google.maps.LatLng(50.087692, 14.421150);
+
+var service = new google.maps.DistanceMatrixService();
+service.getDistanceMatrix(
+  {
+    origins: [origin1, origin2],
+    destinations: [destinationA, destinationB],
+    travelMode: 'DRIVING',
+    transitOptions: TransitOptions,
+    drivingOptions: DrivingOptions,
+    unitSystem: UnitSystem,
+    avoidHighways: Boolean,
+    avoidTolls: Boolean,
+  }, callback);
+
+function callback(response, status) {
+  // See Parsing the Results for
+  // the basics of a callback function.
+
+    if (status == 'OK') {
+      var origins = response.originAddresses;
+      console.log(origins)
+      var destinations = response.destinationAddresses;
+      console.log(desinations)
+  
+      for (var i = 0; i < origins.length; i++) {
+        var results = response.rows[i].elements;
+        for (var j = 0; j < results.length; j++) {
+          var element = results[j];
+          var distance = element.distance.text;
+          console.log(distance)
+          var duration = element.duration.text;
+          console.log(duration)
+          var from = origins[i];
+          var to = destinations[j];
+        }
+      }
+    }
+  }
 
 /*
                 markers.push(marker);
@@ -102,3 +189,9 @@ function initMap() {
                 /*selection.geolocation global variable - pass the lat and lng to the object declared at beginning of code*/
 
                 /*selection.geolocation = location;*/
+
+
+                    /*infoWindow.setPosition(pos);
+                    infoWindow.setContent('Location found.');
+                    infoWindow.open(map);
+                    map.setCenter(pos);*/
